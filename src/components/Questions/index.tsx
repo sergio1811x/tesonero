37b8183587c022ca592b3b13d90ecb802c88ea94
sergio1811x <8qasw8@mail.ru>
2023-03-22 01:19:43 +0300

    fix3

--- src/components/Questions/index.tsx
+++ src/components/Questions/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import RightBlock from './RightBlock';
-import LeftBlock from './LeftBlock';
+import RightBlock from './rightBlock';
+import LeftBlock from './leftBlock';
 import './index.css';
 
 const Questions = () => {
